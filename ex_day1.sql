9780b279be0b4942669c733c956f9e77f3076cd5
Skf1Sky <108626188+Skf1Sky@users.noreply.github.com>
2026-03-05 19:19:21 +0700

    Update ex_day1.sql
    format code

--- ex_day1.sql
+++ ex_day1.sql
@@ -1,24 +1,45 @@
-create database store_managements;
+CREATE DATABASE store_managements;
+
 USE store_managements;
-create table products (
- id int auto_increment primary key,
- name varchar(255) not null,
- des text null,
- price decimal(15,2) null,
- quantity int null 
-);
-insert into products (name,des,price,quantity) 
-values ('Ban Phim','Full Layout',750000,1),('Tai nghe','Tai nghe 7.1',500000,1),('Man hinh','Full HD',1250000,1);
 
-select * from products;
-select name,des from products ;
-update products 
-set price = 800000
-where id = 1 ;
 
-update products 
-set name = 'Ban phim co', price = 850000, quantity=5
-where id =2;
+CREATE TABLE products (id int AUTO_INCREMENT PRIMARY KEY,
+                                             name varchar(255) NOT NULL,
+                                                               des text NULL,
+                                                                        price decimal(15, 2) NULL,
+                                                                                             quantity int NULL);
 
-delete from products where id =1;
-truncate table products; 
+
+INSERT INTO products (name, des, price, quantity)
+VALUES ('Ban Phim','Full Layout',750000,1),
+       ('Tai nghe','Tai nghe 7.1',500000,1),
+       ('Man hinh','Full HD',1250000,1);
+
+
+SELECT *
+FROM products;
+
+
+SELECT name,
+       des
+FROM products ;
+
+
+UPDATE products
+SET price = 800000
+WHERE id = 1 ;
+
+
+UPDATE products
+SET name = 'Ban phim co',
+    price = 850000,
+    quantity=5
+WHERE id =2;
+
+
+DELETE
+FROM products
+WHERE id =1;
+
+
+TRUNCATE TABLE products;
